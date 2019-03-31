f924aadfcfcfff46e65b2536aa37e5f49acd4875
davidlewallen <lewallen.david@gmail.com>
2019-03-31 01:08:29 -0500

    1 - Image was updated, updated reference

--- src/utils/__mock__/websiteRulesMock.js
+++ src/utils/__mock__/websiteRulesMock.js
@@ -6,17 +6,18 @@ module.exports = {
       '1/2 teaspoon garlic salt',
       'ground black pepper to taste',
       '4 skinless, boneless chicken breast halves',
-      '1/2 cup butter, cut into pieces'
+      '1/2 cup butter, cut into pieces',
     ],
     instructions: [
       '1) Preheat oven to 375  degrees F (190 degrees C).',
       '2) Place eggs and cracker crumbs in two separate shallow bowls. Mix cracker crumbs with garlic salt and pepper. Dip chicken in the eggs, then dredge in the crumb mixture to coat.',
       '3) Arrange coated chicken in a 9x13 inch baking dish. Place pieces of butter around the chicken.',
-      '4) Bake in the preheated oven for 40 minutes, or until chicken is no longer pink and juices run clear.'
+      '4) Bake in the preheated oven for 40 minutes, or until chicken is no longer pink and juices run clear.',
     ],
     title: 'Famous Butter Chicken',
     totalTime: '55 minutes',
-    imageURL: 'https://images.media-allrecipes.com/userphotos/560x315/296379.jpg',
+    imageURL:
+      'https://images.media-allrecipes.com/userphotos/560x315/296379.jpg',
     get full() {
       return {
         ingredients: this.ingredients,
@@ -25,7 +26,7 @@ module.exports = {
         totalTime: this.totalTime,
         imageURL: this.imageURL,
       };
-    }
+    },
   },
   foodnetwork: {
     ingredients: [
@@ -54,7 +55,7 @@ module.exports = {
       '12 ounces white candy melting wafers',
       '1 mini (fun-size) peanut-caramel-nougat candy bar',
       '1 round vanilla wafer cookie',
-      '4 ounces dark chocolate'
+      '4 ounces dark chocolate',
     ],
     instructions: [
       '1) For the cakes: Preheat the oven to 325 degrees F.',
@@ -63,15 +64,15 @@ module.exports = {
       '4) Remove 2 2/3 cups batter from the bowl and place it in a separate bowl. Stir in the mint extract and red food color.',
       '5) Spray a 4-cup bundt pan and a 10-cup bundt pan with flour-based baking spray. Alternate layers of vanilla batter and mint (red) batter in each bundt pan until the pans are about 3/4 full. Use a butter knife to gently swirl the two batter colors together.',
       '6) Bake the cupcakes for 15 to 20 minutes, the 4-cup bundt for 30 to 35 minutes and the 10-cup bundt for 50 minutes to 1 hour or until a toothpick tester comes out clean. When the cakes are done, turn them out onto wire racks to cool completely.',
-      '7) When the cupcakes are cool, crumble them into a large bowl and add 2 ounces of cream cheese. Mix together using a rubber spatula or knead together with your hands. Squeeze a small handful of the mixture in your hand. It should hold together. If it doesn\'t and the mixture is crumbly, knead in the remaining 1/2 ounce cream cheese.',
+      "7) When the cupcakes are cool, crumble them into a large bowl and add 2 ounces of cream cheese. Mix together using a rubber spatula or knead together with your hands. Squeeze a small handful of the mixture in your hand. It should hold together. If it doesn't and the mixture is crumbly, knead in the remaining 1/2 ounce cream cheese.",
       '8) Form three roughly ball-shaped pieces from the mixture that are small, medium and large as the segments of a snowman. Flatten the bottom of the largest ball so that it appears to pool and slump to one side. Add the medium ball on top and flatten slightly. Add the smallest ball on top and dowel the three pieces together with a toothpick. Refrigerate until firm, about 15 to 20 minutes.',
       '9) For the glaze and decors: Combine the powdered sugar and vanilla extract in a large bowl. Gradually whisk in the half-and-half a little at a time until a thick white glaze forms. Place the largest bundt cake on a cake stand or serving platter with a lip. Cover the top of the bundt cake with half of the glaze. Top the glazed bunt cake with the second smaller bundt cake. Top the second cake with the remaining glaze. You may not have to use the entire batch of glaze.',
       '10) Cut a small piece away from the orange fruit chew using a knife and mold it into a triangular carrot shape. Break away the point of a toothpick and insert the non-pointed end into the flat side of the carrot shape. Have this ready on hand along with the chocolate chips, pretzel stick, chocolate sprinkles and licorice lace.',
       '11) Melt the white candy melting wafers in the microwave at 100 percent power at 30-second intervals until the candy can be stirred smooth and is pourable.',
       '12) Place the chilled modeled snowman shape on the center of the top bundt cake. Spoon the white melted candy onto the snowman shape and allow the excess to run down the sides of the cakes.',
-      '13) Quickly add two mini chocolate chips (flat side out) for the snowman\'s eyes. Add the two chocolate sprinkles just over the eyes for eyebrows. Press the nose into the face just under the eyes and add the larger chocolate chip (flat side out) for an o-shaped mouth. Break the pretzel stick in half and add to each side of the body, with one arm sticking upright and the other onto the lower part of the body as if it has melted off. Add the three remaining mini chocolate chips for buttons down the front of the snowman body. Add a licorice lace scarf around the neck.',
+      "13) Quickly add two mini chocolate chips (flat side out) for the snowman's eyes. Add the two chocolate sprinkles just over the eyes for eyebrows. Press the nose into the face just under the eyes and add the larger chocolate chip (flat side out) for an o-shaped mouth. Break the pretzel stick in half and add to each side of the body, with one arm sticking upright and the other onto the lower part of the body as if it has melted off. Add the three remaining mini chocolate chips for buttons down the front of the snowman body. Add a licorice lace scarf around the neck.",
       '14) Cut the fun-size candy bar in half and have ready on hand with the vanilla cookie.',
-      '15) Heat the dark chocolate in the microwave at 100 percent power at 30-second intervals until melted and smooth. Dip the cut end of a candy bar half into the chocolate and press it on the center of the cookie, creating a hat shape. Refrigerate until set, about 5 minutes. Dip the entire cookie hat in the chocolate and remove with a fork. Place it on parchment paper and refrigerate until set, about 5 minutes. Dab a small amount of melted chocolate onto the bottom flat part of the cookie hat and hold to the snowman\'s head until set, about 2 minutes.',
+      "15) Heat the dark chocolate in the microwave at 100 percent power at 30-second intervals until melted and smooth. Dip the cut end of a candy bar half into the chocolate and press it on the center of the cookie, creating a hat shape. Refrigerate until set, about 5 minutes. Dip the entire cookie hat in the chocolate and remove with a fork. Place it on parchment paper and refrigerate until set, about 5 minutes. Dab a small amount of melted chocolate onto the bottom flat part of the cookie hat and hold to the snowman's head until set, about 2 minutes.",
       '16) Display as an edible centerpiece at your next holiday gathering. Cut the cake into slices to serve, revealing the red peppermint swirled interior.',
     ],
     title: 'Melting Snowman Cake',
@@ -85,7 +86,7 @@ module.exports = {
         totalTime: this.totalTime,
         imageURL: this.imageURL,
       };
-    }
+    },
   },
   geniuskitchen: {
     ingredients: [
@@ -104,11 +105,12 @@ module.exports = {
       '2) Remove kale leaves from the stem and chop into bite sized pieces (or tear using hands).',
       '3) Place kale in a large bowl and drizzle 1 tablespoon olive oil and salt into leaves, massaging with hands. Do this for a few minutes, massaging the olive oil well into the leaves. Set aside. (Leaves should begin to wilt a little.).',
       '4) Whisk together lemon and orange juice and zest, remaining 1 Tbsp olive oil and honey. Season with a small amount of salt and pepper to taste. Pour dressing over the kale leaves.',
-      '5) Add the avocado, berries and seeds and toss all together. Do not add the dressing, avocado, berries or seeds to the salad until just before serving.'
+      '5) Add the avocado, berries and seeds and toss all together. Do not add the dressing, avocado, berries or seeds to the salad until just before serving.',
     ],
     title: 'Citrusy Kale Salad W/ Blueberries and Pepitas (& Variations)',
     totalTime: '20 minutes',
-    imageURL: 'https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/46/25/94/MwrTLl9tTi5LzTfU9XdA_kale%20salad-9820.jpg',
+    imageURL:
+      'https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/46/25/94/MwrTLl9tTi5LzTfU9XdA_kale%20salad-9820.jpg',
     get full() {
       return {
         ingredients: this.ingredients,
@@ -117,10 +119,11 @@ module.exports = {
         totalTime: this.totalTime,
         imageURL: this.imageURL,
       };
-    }
+    },
   },
   myrecipes: {
-    ingredients: ['2 cups old-fashioned rolled oats',
+    ingredients: [
+      '2 cups old-fashioned rolled oats',
       '2 cups sliced almonds',
       '1 cup pumpkin seeds',
       '1/2 cup hempseed',
@@ -132,15 +135,17 @@ module.exports = {
       '1/2 cup maple syrup',
       '1/4 cup extra-virgin olive oil',
       '3 tablespoons coconut oil, melted and cooled',
-      '1/2 cup goji berries'],
+      '1/2 cup goji berries',
+    ],
     instructions: [
       '1) Preheat oven to 300ºF. Toss together oats, almonds, pumpkin seeds, hempseed, coconut, cinnamon, ginger, turmeric, and salt in a large bowl. Stir in syrup, olive oil, and coconut oil.',
       '2) Divide mixture between 2 large rimmed baking sheets and spread out evenly. Bake until golden and toasted, 40 to 45 minutes, stirring every 10 to 15 minutes and switching pans from top to bottom and front to back halfway through.',
-      '3) Immediately transfer granola to a large bowl and let cool completely, stirring occasionally. (Granola will crisp as it cools.) When it\'s cool, toss in goji berries. Store leftovers in an airtight container for up to 2 weeks.'
+      "3) Immediately transfer granola to a large bowl and let cool completely, stirring occasionally. (Granola will crisp as it cools.) When it's cool, toss in goji berries. Store leftovers in an airtight container for up to 2 weeks.",
     ],
     title: 'Superfood Crunch Granola',
     totalTime: '1 hour',
-    imageURL: 'https://cdn-image.myrecipes.com/sites/default/files/styles/medium_2x/public/1504900580/superfood%20crunch%20granola_0.jpg?itok=Spa64xMs',
+    imageURL:
+      'https://cdn-image.myrecipes.com/sites/default/files/styles/medium_2x/public/1504900580/superfood%20crunch%20granola_0.jpg?itok=Spa64xMs',
     get full() {
       return {
         ingredients: this.ingredients,
@@ -149,10 +154,11 @@ module.exports = {
         totalTime: this.totalTime,
         imageURL: this.imageURL,
       };
-    }
+    },
   },
   realsimple: {
-    ingredients: ['1 ounce dried porcini mushrooms',
+    ingredients: [
+      '1 ounce dried porcini mushrooms',
       '8 cups unsalted beef stock, divided',
       '2 tablespoons canola oil',
       '1½ pounds beef stew meat',
@@ -165,14 +171,18 @@ module.exports = {
       '2 cups chopped carrots (about 6 oz.)',
       '2 cups quartered Brussels sprouts (about 4 oz.)',
       '½ teaspoon black pepper',
-      '2 tablespoons chopped fresh flat-leaf parsley'],
-    instructions: ['1) Combine mushrooms and 2 cups of the stock in a saucepan. Bring to a boil over high; remove from heat, and let stand 45 minutes to 1 hour.',
+      '2 tablespoons chopped fresh flat-leaf parsley',
+    ],
+    instructions: [
+      '1) Combine mushrooms and 2 cups of the stock in a saucepan. Bring to a boil over high; remove from heat, and let stand 45 minutes to 1 hour.',
       '2) Meanwhile, heat oil in a large Dutch oven over high. Sprinkle beef with 1 teaspoon of the salt. Cook beef in hot oil, turning occasionally, until browned on all sides, 8 to 10 minutes. Remove from Dutch oven. Add onion, thyme, and garlic to Dutch oven, and cook, stirring and scraping to loosen browned bits from bottom of Dutch oven, until onion is translucent, 3 to 4 minutes. Add remaining 6 cups stock, and bring to a boil. Reduce heat to medium-low, and add browned beef. Cover and simmer 45 minutes.',
       '3) Pour mushroom mixture through a fine wire-mesh strainer into a bowl, reserving liquid. Finely chop mushrooms.',
-      '4) Add barley, chopped mushrooms, reserved mushroom liquid, and remaining 1 teaspoon salt to beef mixture. Increase heat to medium-high, and return to a boil. Reduce heat to medium-low, and simmer 30 minutes. Add rutabaga and carrots, and simmer until barley, vegetables, and beef are tender, 20 to 25 minutes. Add Brussels sprouts and pepper, and cook until Brussels sprouts are just tender, about 5 minutes. Sprinkle servings with parsley.'],
+      '4) Add barley, chopped mushrooms, reserved mushroom liquid, and remaining 1 teaspoon salt to beef mixture. Increase heat to medium-high, and return to a boil. Reduce heat to medium-low, and simmer 30 minutes. Add rutabaga and carrots, and simmer until barley, vegetables, and beef are tender, 20 to 25 minutes. Add Brussels sprouts and pepper, and cook until Brussels sprouts are just tender, about 5 minutes. Sprinkle servings with parsley.',
+    ],
     title: 'Beef-and-Barley Soup With Porcini',
     totalTime: '2 hours 10 minutes',
-    imageURL: 'https://cdn-image.realsimple.com/sites/default/files/styles/portrait_435x518/public/1483995143/beef-barley-soup.jpg?itok=Rx_saul7',
+    imageURL:
+      'https://cdn-image.realsimple.com/sites/default/files/styles/marquee_large_2x/public/1483995143/beef-barley-soup.jpg?itok=ikHwSHmz',
     get full() {
       return {
         ingredients: this.ingredients,
@@ -181,13 +191,17 @@ module.exports = {
         totalTime: this.totalTime,
         imageURL: this.imageURL,
       };
-    }
+    },
   },
   health: {
-    ingredients: ['3 tablespoons fresh mint leaves',
+    ingredients: [
+      '3 tablespoons fresh mint leaves',
       '1/2  English cucumber (about 7 oz.), peeled and chopped',
-      '  Pinch of sea salt'],
-    instructions: ['1) Place all ingredients in a blender with 4 cups cool water; blend until smooth. Refrigerate for 30 minutes. Strain through a fine-mesh sieve into glasses and serve, or refrigerate to serve later.'],
+      '  Pinch of sea salt',
+    ],
+    instructions: [
+      '1) Place all ingredients in a blender with 4 cups cool water; blend until smooth. Refrigerate for 30 minutes. Strain through a fine-mesh sieve into glasses and serve, or refrigerate to serve later.',
+    ],
     title: 'Cucumber Mint Tonic',
     totalTime: 'n/a',
     imageURL: '',
@@ -199,6 +213,6 @@ module.exports = {
         totalTime: this.totalTime,
         imageURL: this.imageURL,
       };
-    }
-  }
+    },
+  },
 };
