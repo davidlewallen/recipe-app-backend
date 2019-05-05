271d38641795fadcf072983b324699f9bfe804ca
davidlewallen <lewallen.david@gmail.com>
2019-05-05 18:45:34 -0500

    updates to eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'API',
+    name: 'prod',
     script: 'npm',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
@@ -9,12 +9,14 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
+  }, {
+    name: 'beta',
+    script: 'npm',
+    args: 'run start-beta',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '16',
   }],
 
 };
